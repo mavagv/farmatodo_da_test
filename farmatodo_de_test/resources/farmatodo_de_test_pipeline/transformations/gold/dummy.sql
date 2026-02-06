236b3dc9657a88e6849c5b94c3af27b5a52621cd
mavagv <mavagonzalez05@gmail.com>
2026-02-06 18:21:53 -0400

    Eliminacion de carpeta no relacionada a la prueba

--- farmatodo_de_test/resources/farmatodo_de_test_pipeline/transformations/gold/dummy.sql
+++
@@ -1,6 +0,0 @@
--- Please edit the sample below
-
-CREATE OR REFRESH MATERIALIZED VIEW workspace.operations.dummy_gold AS
-SELECT
-    *
-FROM workspace.operations.dummy_silver;
--- farmatodo_de_test/resources/farmatodo_de_test_pipeline/transformations/silver/dummy.sql
+++
@@ -1,7 +0,0 @@
--- Please edit the sample below
-
-CREATE OR REFRESH MATERIALIZED VIEW workspace.operations.dummy_silver AS
-SELECT
-    pickup_zip,
-    fare_amount
-FROM samples.nyctaxi.trips;
